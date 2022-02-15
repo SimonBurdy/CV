5b3f5103e3067887293dc13617a2fb2d6d432d7a
SimonBurdy <simon.burdy@lacatholille.fr>
2022-02-15 22:48:42 +0100

    config ressources

---
+++ resources/lang/fr/billing.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Utilisé lors des devis / facture
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during billing
+    |
+    */
+    'piece(s)' => 'pièce(s)',
+    'hour(s)' => "heure(s)",
+    'day(s)' => 'jour(s)',
+    'week(s)' => 'semaine(s)',
+    'month(s)' => 'mois',
+    'year(s)' => 'année(s)',
+    'point(s)' => 'point(s)',
+    'draft' => 'Brouillon',
+    'waiting for payment' => 'En Attente de paiement',
+    'unpaid invoice' => 'Facture impayée',
+    'partially paid' => 'Partiellement payé',
+    'to be paid upon receipt' => 'À payer à reception',
+    'to be paid when due' => 'À payer à échéance',
+    'direct debit' => 'Prélèvement automatique',
+    'paid in credit card' => 'Payé en CB',
+    'paid by bank transfer' => 'Payé par virement',
+    'paid' => 'Payé'
+];
