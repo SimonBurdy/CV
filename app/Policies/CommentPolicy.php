e933ab598f19550bc91f1bcf7f2634156e841e27
SimonBurdy <simon.burdy@lacatholille.fr>
2022-02-15 22:46:25 +0100

    policy

---
+++ app/Policies/CommentPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CommentPolicy
+{
+    use HandlesAuthorization;
+
+
+
+    /**
+     * Determine whether the user can update the comment.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Models\Comment  $comment
+     * @return mixed
+     */
+    public function update(User $user, Comment $comment)
+    {
+        return $user->id === $comment->user_id;
+    }
+
+}
